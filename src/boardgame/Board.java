80df6f911dc466d7a776ad0d8912887ca149769e
tiagorobert8 <tiagorobert8@gmail.com>
2021-03-18 17:06:30 -0300

    Correção tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,13 +1,11 @@
-
 package boardgame;
-
 
 public class Board {
     private int rows;
     private int columns;
     private Piece[][] pieces;
 
-    public Board(int row, int columns) {
+    public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];  
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,4 @@
-
 package boardgame;
-
 
 public class Piece {
     
