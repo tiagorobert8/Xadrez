071b780f3361d2fdbf8f05812534e0050d335800
tiagorobert8 <tiagorobert8@gmail.com>
2021-03-18 14:10:27 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,16 @@
 
 package application;
 
+import boardgame.Position;
+
 
 public class Program {
 
     
     public static void main(String[] args) {
      
-         System.out.println("HEllo Wold");
+         Position pos = new Position(3, 5);
+         System.out.println(pos);
     }
     
 }
